9c0b9d4a8e1e8e20beb3a345e4391d9d05ac5613
cocaine <cocainexcix@gmail.com>
2024-05-21 15:59:38 +0545

    main

---
+++ 626c6f672d617279616e/6d61696e/6a73/636f6d6d616e6473.js
@@ -0,0 +1,85 @@
+
+var twitter = "https://www.twitter.com/arisksec/";
+var password = "Return";
+var github = "https://github.com/arisksec/";
+var email = 'mailto:me@arisksec.com';
+var blog = "https://blog.aryanaryal.com.np";
+
+whois = [
+  "<br>",
+  "Hey, I'm ARYAN!👋",
+  "I'm a Application security researcher, Bug Bounty hunter, hacker and webapp developer",
+  "who builds engaging open source tools like this one and research about web and mobile",
+  "app security technique.",
+  "I worked professionally security research, While doing all of that, I documentned my",
+  "research journey on Blog - trying to enlighten",
+  "the next generation of hacker and help them navigate the crazy world that is security", 
+  "technique development & computer science.", 
+  "Before I knew it, that online presence took on a life of its own, to the point where I knew", 
+  "I needed to make the jump from Appsec research to full time Bug Bounty hunter, and it's", 
+  "the best decision I ever made.", "'Decoded w/ Aryan Aryal.' What most people don't know",
+  " and will only know because they're reading this right now, is that I also run a creative.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  
+  ' Twitter      <a href="' + twitter + '" target="_blank">ariksec' + '</a>',
+  ' Github       <a href="' + github + '" target="_blank"> ariksec' + "</a>",
+  ' Blog         <a href="' + blog + '" target="_blank">ariksec' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Forrest?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">ARYANARYAL (ARISKSEC) Not A Corporation. All rights reserved.</span>',
+  "						                                                     ",                                        
+  "   5#:                                                                                           ",
+  "   JG.       ??                                                                                  ",
+  "   JP.       PP  .                                                                               ",
+  "GB.YB:      .BG Y#^     G7   YP.7P..5G. 7^  ~B.  ~GJ  ?:     5Y   ?G.!P..JB: !~  ^B:  .GJ        ",
+  "Y5 JG.^Y:.Y~ 55 7G.    !7&~  5#^GY   GG~.  ^~GB  .!?B~!.    !~&?  ?&^PP   YB~.  .!5#.  &J      ",  
+  "55 !Y.~G^.G7 ?J 7G.   ?: J&: P#:5G:  ^@!  :? .#P ^7 .G#    7^ !&~ J&:YB^  .&Y  .?..BB..&Y  ! ",   
+  "BB    7#~:#Y    5#:  ..   .. ..  ..  ...  ..  .....   .    .   .. ..  ..  ...  ..  .........",    
+  "..    ~B^.G?     .                                                                   © 2024",                                                                               
+  "    . ~5:.5!",                                                                                   
+  "   P&:      .#B",                                                                                 
+  "    .        .",   
+  "              ",   
+
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
