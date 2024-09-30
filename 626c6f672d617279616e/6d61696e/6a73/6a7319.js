fd0521ed2d5f41ebf969a6973f2bbaf27b0ab47b
cocaine <cocainexcix@gmail.com>
2024-10-01 01:13:07 +0545

    Dir rename
    rename

---
+++ 626c6f672d617279616e/6d61696e/6a73/6a7319.js
@@ -0,0 +1,31 @@
+
+
+
+
+          <!-- JSON-LD structured data -->
+			<script type="application/ld+json">
+				{
+				  "@context": "http://schema.org",
+				  "@type": "Person",
+				  "name": "Aryan Aryal",
+				  "url": "https://www.aryanaryal.com.np",
+				  "sameAs": [
+				    "https://www.twitter.com/arisksec/",
+				    "https://blog.aryanaryal.com.np/",
+				    "https://github.com/arisksec/"
+				  ]
+				}
+			</script>
+
+
+
+			  // Set cookie securely using Secure and SameSite flags (HttpOnly is server-side only)
+			  document.addEventListener('DOMContentLoaded', function() {
+			    // This cookie will only be sent over HTTPS and will not be sent cross-site
+			    document.cookie = "username=arisksec; Secure; SameSite=Strict";
+			  });
+
+			
+
+
+
